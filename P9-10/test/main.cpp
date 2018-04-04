cfd27ac032de15f0be86e6c6dc86dab41453ae9e
Thomas <tkjj13@student.aau.dk>
2018-04-04 10:03:02 +0200

    long thesis start

---
+++ P9-10/test/main.cpp
@@ -0,0 +1,31 @@
+
+
+
+
+// #include <unistd.h>
+#include <stdio.h>
+// #include <stdlib.h>
+// #include <signal.h>
+// #include <pthread.h>
+
+#include <iostream>
+// #include <fstream>
+// #include <string>
+// #include <boost/program_options.hpp>
+// #include <boost/program_options/parsers.hpp>
+
+using namespace std;
+//namespace bpo = boost::program_options;
+
+int main(int argc, char const *argv[])
+{
+	 // bpo::options_description general("General options");
+	 // general.add_options()
+  //     ("help,h", "Produce help message")
+  //     ("version,v", "Print version information and exit")
+  //     ;
+
+	//cout << "----  Software Radio Systems LTE UE - AAU Massive MTC Edition  ----" << endl;
+	printf("test \n");
+	return 0;
+}
